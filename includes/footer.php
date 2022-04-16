f59e5d50cc3f56cb0735b4e414bb790545f0b993
Fogap94 <r0769037@student.thomasmore.be>
2022-04-16 14:12:32 +0200

    AboutUsPAGE

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
                 <li class="list-inline-item"><a href="index.php">Home</a></li>
                 <li class="list-inline-item"><a href="contact.php" target="_blank">CONTACT US</a></li>
                 <li class="list-inline-item"><a href="#">Services</a></li>
-                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="AboutUs.php">About</a></li>
                 <li class="list-inline-item"><a href="#">Terms</a></li>
                 <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
             </ul>
