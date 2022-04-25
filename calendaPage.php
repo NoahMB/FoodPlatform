ba22af9b9d86552d21785754cd80d9be8e0ef556
Fogap94 <r0769037@student.thomasmore.be>
2022-04-25 22:46:29 +0200

    DesingOnTheCalendarPage

--- calendaPage.php
+++ calendaPage.php
@@ -149,6 +149,10 @@ function closeForm1() {
 </script>
 
 <?include_once 'includes/footer.php';?> 
+<br>
+<br>
+<br>
+<br>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
 </body>
