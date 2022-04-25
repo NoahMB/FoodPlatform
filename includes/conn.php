5d4e9a695cbd608fb768c9b6a977ee1127ac0b94
Fogap94 <r0769037@student.thomasmore.be>
2022-04-25 22:43:44 +0200

    ConnectingToDatabase

--- includes/conn.php
+++ includes/conn.php
@@ -1,6 +1,6 @@
+
 <?php
 
-//Connect to database
 
 $servername = "127.0.0.1";
 $root = "root";
@@ -8,7 +8,9 @@ $password = "";
 
 $conn = new mysqli($servername, $root, $password, "Kaddoo");
 
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
