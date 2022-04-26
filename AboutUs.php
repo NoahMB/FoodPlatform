db2fabf02e75ec501c7b543c4a1852fee0c28758
Fogap94 <r0769037@student.thomasmore.be>
2022-04-26 23:10:48 +0200

    AboutUsPageContent

--- AboutUs.php
+++ AboutUs.php
@@ -6,14 +6,44 @@
     
     <div class="AboutUsContent">
       
-      <h1 >
-          ABOUTUS KADDOO
+      <h1 id="AboutUsHeader">
+          ABOUTUS 
       </h1>
+      <div class ="SignUP_C">
+          <div class = "AboutUsContent">
+            <p>
+            There's nothing better than a wide-eyed smile on the face of your loved ones when you give them unexpected small gifts out of the ordinary. And that's exactly what led to the birth of Kaddoo.in Leaving behind the tradition of conventional gifting, we decided to make it more than just another giveaway. We help you give your near and dear ones something that goes with their persoanality and interest, looking at which they’d say ”Yes, that’s me!”. Hence, with our aim to get you the best presents from across the world, we at bigsmall.in have embarked on a journey to make gifting a hassle-free experience. Thus, you'll never find an ordinary thing here. From products to packaging and customer service, every little thing has its personal touch. That's what makes bigsmall.in extraordinary and different from your average stores!
+            </p> 
+            <br>
+            <br>
+            <h2>
+                what makes Us different ?
+            </h2> 
+            <p>
+            We are on a mission to make uncommon and awesome gifts accessible to everyone sitting at home. With a few clicks over the internet, your experience will be right up at your door. But, we also realise how much every gift means to you. And that is why we make sure, that it is a memorable experience!
+            </p>
+            <br>
+            <br>
+            <br>
+            <br>
+            <br>  
+            <h2>
+                About Our team
+            </h2>
+            <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          <br>
+          </div>
+    
+      </div>
     
     </div>
-
-
-
+    <br>
     <?php include_once 'includes/footer.php';?> 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
