a3bda48ae0d4fcf50e01392f5dacbec384c2fea9
Noah MB <morgantebormansnoah@gmail.com>
2022-05-03 20:22:39 +0200

    Fixed connection issues

--- includes/conn.php
+++ includes/conn.php
@@ -1,14 +1,13 @@
 <?php
- $HostName = "localhost";
- $dbUSERname ="root";
- $dbpassword = "root";
- $dbName = "kaddoo";
- $dbPort = "8889";
-
- $conn = mysqli_connect($HostName, $dbUSERname, $dbpassword, $dbName,  $dbPort);
 
 
-// Check connection
+$servername = "127.0.0.1";
+$root = "root";
+$password = "";
+
+$conn = new mysqli($servername, $root, $password, "Kaddoo");
+
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
