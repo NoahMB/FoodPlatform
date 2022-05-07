c40bc72e730722119c73462af8cd37d2783fc2a3
TotallyDone <54904876+TotallyDone@users.noreply.github.com>
2022-05-07 15:45:18 +0200

    webshop

---
+++ webshop.php
@@ -0,0 +1,21 @@
+<?php include_once 'includes/header.php';?> 
+    <title>webshop</title>
+</head>
+<body>
+<?php include_once 'includes/nav.php';
+$json = file_get_contents('C:\xampp\htdocs\Kaddoo\includes\Python\search_results_output.json', true);
+$data = json_decode($json, true);
+//echo $data;
+#var_dump($data);
+foreach ($data['items'] as $address)
+{
+    if($address['price'] != null){
+        echo "<div><a href='https://www.amazon.com" . $address['url']. "' target = '_blank'>";
+        echo  $address['title'] ." ";
+        echo  $address['rating']." ";
+        echo  $address['reviews']." ";
+        echo  $address['price'] ." ";
+        echo "<img src='". $address['image'] ." '> </a></div></br>";
+    }
+};
+?>
