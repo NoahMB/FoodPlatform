03c0523a4206f82ab241574d0797971b65c038b6
TotallyDone <54904876+TotallyDone@users.noreply.github.com>
2022-05-07 17:44:34 +0200

    create text file with id of intrest

--- webshopRedirect.php
+++ webshopRedirect.php
@@ -1,7 +1,30 @@
+
+
+
 <?php
+ include_once 'includes/header.php';
 
-exec("python includes/Python/searchresults.py");
 
+if (isset($_GET["id"])) {
+    $eventid = $_GET['id'];
+    $sql = "SELECT FriendsID FROM events WHERE EventsID = ". $eventid;
+    #echo $sql;
+    require_once 'includes/conn.php';
+    $result = mysqli_query($conn , $sql);
+    while ($row = $result->fetch_assoc()) {
+        $sql2 = "SELECT InterestsID FROM friendsinterests WHERE FriendsID =". $row['FriendsID'];
+        $result2 = mysqli_query($conn , $sql2);
+
+            while ($row2 = $result2->fetch_assoc()) {
+                $filename = $_SESSION["AccountsID"] ."_urls.txt";
+                $myfile = fopen($filename, "w");
+                $txt = $row2['InterestsID'];
+                fwrite($myfile, $txt);
+            }
+    }
+}
+echo "<p hidden class='hidden'>" . $_SESSION["AccountsID"] . "</p>";
 
 
 ?>
+
