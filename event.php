945bce3f24421a35b3997f3ae06edbe1f973c276
Fogap94 <r0769037@student.thomasmore.be>
2022-05-21 10:42:23 +0200

    EventFavIconAdded

--- event.php
+++ event.php
@@ -1,6 +1,7 @@
 <?php include_once 'includes/header.php'; ?>
     
     <title>Events</title>
+      <link rel="shortcut icon" type="icon" href ="Image/Event.ico">
     <link type="image/png" sizes="16x16" rel="icon" href=".../icons8-shopping-cart-16.png">
 </head>
 <style>
