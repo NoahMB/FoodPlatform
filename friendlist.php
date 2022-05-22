c241a2010bd98b8825856eb0194fd4fb190a90a0
Fogap94 <r0769037@student.thomasmore.be>
2022-05-22 21:07:12 +0200

    MulitpleInterestOnTableFixed

--- friendlist.php
+++ friendlist.php
@@ -61,10 +61,11 @@ tr:last-child td:last-child { border-bottom-right-radius: 10px; }
 
                             $sql2 = "SELECT * FROM interests WHERE InterestsID in (SELECT InterestsID from friendsinterests where FriendsID=".$row["FriendsID"].")";
                             $result2= mysqli_query($conn , $sql2);
-                            
+                            echo   "<td>";
                             while ($row2 = $result2->fetch_assoc()) {
-                                echo   "<td>" . $row2['Interests'] . "</td>";
+                                 echo $row2['Interests'] ; echo '<br>';
                             }
+                            "</td>";
                     echo"<td><a href=''><svg xmlns='http://www.w3.org/2000/svg' class='h-6 w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor' stroke-width='2'>
                     <path stroke-linecap='round' stroke-linejoin='round' d='M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z' />
                     </svg></a></td>";
